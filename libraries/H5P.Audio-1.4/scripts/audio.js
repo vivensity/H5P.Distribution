344ec6f127a06b0b779dd7a9f798715be526bbf7
Muhammad Basit Tkxel <muhammad.basit@tkxel.com>
2022-04-08 15:38:19 +0500

    added audio and exportable text are consumed

--- libraries/H5P.Audio-1.4/scripts/audio.js
+++ libraries/H5P.Audio-1.4/scripts/audio.js
@@ -312,9 +312,9 @@ H5P.Audio.prototype.stop = function () {
  * Play
  */
 H5P.Audio.prototype.play = function () {
-  // self.triggerXAPI('interacted');
-  self.triggerXAPI('consumed');
-  self.triggerXAPIConsumed();
+  // this.triggerXAPI('interacted');
+  this.triggerXAPI('consumed');
+  this.triggerXAPIConsumed();
   if (this.flowplayer !== undefined) {
     this.flowplayer.play();
   }
