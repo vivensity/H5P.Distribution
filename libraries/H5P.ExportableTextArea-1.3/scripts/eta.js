d2b876b5194931dc33bcb262a479ec47dfd76776
Muhammad Basit Tkxel <muhammad.basit@tkxel.com>
2022-04-11 15:31:48 +0500

    added audio and exportable text are consumed

--- libraries/H5P.ExportableTextArea-1.3/scripts/eta.js
+++ libraries/H5P.ExportableTextArea-1.3/scripts/eta.js
@@ -51,20 +51,8 @@ H5P.ExportableTextArea = (function ($, EventDispatcher) {
     }
   };
 
-  const customEventInteract =H5P.externalDispatcher.createXAPIEventTemplate("interacted");
-  if (customEventInteract.data.statement.object) {
-    customEventInteract.data.statement.object.definition["description"] = {
-      "en-US":this.contentData.metadata.title
-    };
-    customEventInteract.data.statement.object.definition["name"] ={
-      "en-US":this.contentData.metadata.title
-    };
-    customEventInteract.data.statement.object["objectType"] ="Activity";
-    customEventInteract.data.statement.object["id"] ="http://adlnet.gov/expapi/activities"
-    self.trigger(customEventInteract);
-  }
-    // this.triggerXAPI('interacted');
-  // self.triggerXAPIConsumed();
+  this.triggerXAPI('interacted');
+  this.triggerXAPIConsumed();
 
   return C;
 })(H5P.jQuery, H5P.EventDispatcher);
