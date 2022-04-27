9d97402069028f15a0da588914011cd122ae5573
Yuvaraj Sanjeevi <yuvaraj@curriki.org>
2022-04-27 12:20:57 +0530

    Reverted CUR-1744 - Math Column: Preview: IFrame Embedder: The content of the h5p is still cropped although "Resize supported" is selected

--- libraries/H5P.IFrameEmbed-1.0/iframe-embed.js
+++ libraries/H5P.IFrameEmbed-1.0/iframe-embed.js
@@ -73,7 +73,6 @@ H5P.IFrameEmbed = function (options, contentId, contentData) {
       $iframe.css(
         (H5P.isFullscreen) ? {width: '100%', height: '100%'} : getElementSize($iframe)
       );
-      setIframeHeight($iframe)
     }
   };
 
@@ -87,9 +86,9 @@ H5P.IFrameEmbed = function (options, contentId, contentData) {
     // the parent. Make sure 'element' doesn't scale below
     // 'options.minWidth'.
     var elementMinWidth = parseInt(options.minWidth ,10);
-    var document = $iframe[0].contentDocument? $iframe[0].contentDocument: $iframe[0].contentWindow.document;
-    var height = getDocHeight(document);
-    console.log('iframe height', height);
+    // var document = $iframe[0].contentDocument ? $iframe[0].contentDocument: $iframe[0].contentWindow.document;
+    // var height = getDocHeight(document);
+    // console.log('iframe height', height);
     var elementSizeRatio = parseInt(options.height, 10) / parseInt(options.width, 10);
     var parentWidth = $element.parent().width();
     var elementWidth = (parentWidth > elementMinWidth) ? parentWidth : elementMinWidth;
