33fa3efe088e28cbc503144de8da1b1af65fd81c
Yuvaraj Sanjeevi <yuvaraj@curriki.org>
2022-05-05 23:49:25 +0530

    CUR-3651 - Issue #2: CL>CP>Read only activity should remove "View summary" button

--- libraries/H5P.Column-1.13/scripts/h5p-column-with-pagination.js
+++ libraries/H5P.Column-1.13/scripts/h5p-column-with-pagination.js
@@ -263,7 +263,9 @@ H5P.Column = (function (EventDispatcher) {
     var showSummary = function () {
       for (const inst of instances) {
         const machineName = inst.libraryInfo.machineName;
-        if (!readOnlyActivities.includes(machineName)) {
+        if (!readOnlyActivities.includes(machineName)
+            || ( ['H5P.InteractiveVideo', 'H5P.CoursePresentation'].includes(machineName) && Column.isTask(inst))
+        ) {
           return true;
         }
       }
@@ -631,7 +633,8 @@ H5P.Column = (function (EventDispatcher) {
 
           // Do not show read only activities in summary
           const machineName = inst.libraryInfo.machineName;
-          if (readOnlyActivities.includes(machineName)) {
+          if (readOnlyActivities.includes(machineName)
+              || (['H5P.InteractiveVideo', 'H5P.CoursePresentation'].includes(machineName) && !Column.isTask(inst)) ) {
             i++;
             continue;
           }
